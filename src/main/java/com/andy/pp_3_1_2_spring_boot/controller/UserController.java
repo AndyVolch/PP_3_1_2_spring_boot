a244114ebf7f1d3794d7ff318317e992e24ba3b0
AndyVolch <andrewvoloshin520@gmail.com>
2023-03-02 09:20:35 +1000

    Убраны @Aurowired над конструкторами

--- src/main/java/com/andy/pp_3_1_2_spring_boot/controller/UserController.java
+++ src/main/java/com/andy/pp_3_1_2_spring_boot/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class UserController {
     private final UserService userService;
-    @Autowired
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
